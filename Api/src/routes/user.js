08189b7eb699b75a76f72e52eba1ddf26e80a546
Guido <levyguidocarp@gmail.com>
2023-04-10 14:57:11 -0300

    route get one user

--- Api/src/routes/user.js
+++ Api/src/routes/user.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const {User} = require("../db.js");
 const router = Router()
 
+//get all
 router.get('/' , async(req,res)=>{
     try {
         const allUser = await User.findAll();
@@ -11,26 +12,25 @@ router.get('/' , async(req,res)=>{
     }
 })
 
-router.get('/', async(req,res)=>{
+//get one
+router.get('/:id', async(req,res)=>{
     try {
-        
+        const {id} = req.params;
+        const oneUser = await User.findAll({
+            where:{
+                id:id
+            }
+        });
+        if(!oneUser[0].dataValues.name){
+            throw Error()
+        }
+        res.status(200).send(oneUser)
     } catch (error) {
-        
+        res.status(404).send(error.message)
     }
 })
 
-router.delete('/:id', async (req,res) =>{
-    const {id} = req.params
-    console.log(id);
-    try {
-        const deletedUser = await User.findAll({id:id})
-        await User.destroy({where:{id:id}})
-        res.status(200).send(deletedUser);
-    } catch (error) {
-        res.status(404).send(error.message);
-    }
-})
-
+//post one
 router.post("/", async (req,res) => {
     try {
         const {email , name ,last_name ,image} = req.body;
@@ -41,6 +41,20 @@ router.post("/", async (req,res) => {
         res.status(400).send(error.message)
     }
     
+})
+
+
+//delete one
+router.delete('/:id', async (req,res) =>{
+    const {id} = req.params
+    console.log(id);
+    try {
+        const deletedUser = await User.findAll({id:id})
+        await User.destroy({where:{id:id}})
+        res.status(200).send(deletedUser);
+    } catch (error) {
+        res.status(404).send(error.message);
+    }
 })
 
 module.exports = router;
